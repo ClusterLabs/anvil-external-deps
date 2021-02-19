e83caee999dce663772ffd6ceadff0ff1f56aa35
Fabio M. Di Nitto <fabbione@fabbione.net>
2021-02-19 20:24:39 +0100

    Fix source dir
    Signed-off-by: Fabio M. Di Nitto <fabbione@fabbione.net>

--- srpms/perl-Log-Journald/build.sh
+++ srpms/perl-Log-Journald/build.sh
@@ -4,5 +4,5 @@ set -e
 
 rm -f *.tar.gz
 wget $(spectool -S perl-Log-Journald.spec | grep ^S | awk '{print $NF}')
-rpmbuild --define "_srcrpmdir $(pwd)" --nodeps -bs perl-Log-Journald.spec
+rpmbuild --define "_srcrpmdir $(pwd)" --define "_sourcedir $(pwd)" --nodeps -bs perl-Log-Journald.spec
 rm -f *.tar.gz
