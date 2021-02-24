7c07531b798d0326fae430531313b981a859d317
Fabio M. Di Nitto <fabbione@fabbione.net>
2021-02-24 08:32:22 +0100

    Fix download method
    Signed-off-by: Fabio M. Di Nitto <fabbione@fabbione.net>

--- srpms/drbd90-kmod/build.sh
+++ srpms/drbd90-kmod/build.sh
@@ -3,6 +3,7 @@
 set -e
 
 rm -f *.tar.gz
-wget $(spectool --define "_srcrpmdir $(pwd)" --define "_sourcedir $(pwd)" -S drbd90-kmod.spec | grep "^Source0:" | awk '{print $NF}')
+# spectool on centos doesn´t like our spec file (works on fedora)
+wget https://www.linbit.com/downloads/drbd/9.0/drbd-9.0.27-1.tar.gz
 rpmbuild --define "_srcrpmdir $(pwd)" --define "_sourcedir $(pwd)" --nodeps -bs drbd90-kmod.spec
 rm -f *.tar.gz
