8b050efa8931156d96a88c799d7b731055f53d95
Fabio M. Di Nitto <fabbione@fabbione.net>
2021-05-23 07:54:52 +0200

    Drop obsolete drbd90-kmod
    Signed-off-by: Fabio M. Di Nitto <fabbione@fabbione.net>

--- srpms/drbd90-kmod/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-rm -f *.tar.gz
-# spectool on centos doesn´t like our spec file (works on fedora)
-wget https://www.linbit.com/downloads/drbd/9.0/drbd-9.0.28-1.tar.gz
-rpmbuild --define "_srcrpmdir $(pwd)" --define "_sourcedir $(pwd)" --nodeps -bs drbd90-kmod.spec
-rm -f *.tar.gz
