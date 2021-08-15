44b5d28a9dbf270e3544d694e8eb124eb15e0eb0
Fabio M. Di Nitto <fabbione@fabbione.net>
2021-08-15 06:54:37 +0200

    Obsolete old drbd-kmod packaging
    Signed-off-by: Fabio M. Di Nitto <fabbione@fabbione.net>

--- srpms/drbd91-kmod/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-rm -f *.tar.gz
-# spectool on centos doesn´t like our spec file (works on fedora)
-wget https://linbit.com/downloads/drbd/9/drbd-9.1.2.tar.gz
-rpmbuild --define "_srcrpmdir $(pwd)" --define "_sourcedir $(pwd)" --nodeps -bs drbd91-kmod.spec
-rm -f *.tar.gz
