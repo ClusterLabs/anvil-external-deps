45436a38b4b58eef1bac15f2d570a1b78836ba5e
Fabio M. Di Nitto <fabbione@fabbione.net>
2024-08-17 16:36:14 +0200

    Update to new drbd kmod and utils for rhel 9 / alma 9
    Signed-off-by: Fabio M. Di Nitto <fabbione@fabbione.net>

---
+++ srpms/almalinux-9/drbd-kmod/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+rm -f *.tar.gz
+# spectool on centos doesn´t like our spec file (works on fedora)
+wget https://pkg.linbit.com//downloads/drbd/9/drbd-9.2.11.tar.gz
+rpmbuild --define "_srcrpmdir $(pwd)" --define "_sourcedir $(pwd)" --nodeps -bs drbd-kmod.spec
+rm -f *.tar.gz
