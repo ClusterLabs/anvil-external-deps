a8e237bb8d55af9178c485a5875c37737004ceb9
Fabio M. Di Nitto <fabbione@fabbione.net>
2024-12-02 05:59:22 +0100

    Drop centos-8 and rhel-8
    Signed-off-by: Fabio M. Di Nitto <fabbione@fabbione.net>

--- srpms/rhel-8/python-redis/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-rm -f *.tar.gz
-wget $(spectool --define "_srcrpmdir $(pwd)" --define "_sourcedir $(pwd)" -S python-redis.spec | grep "^Source0:" | awk '{print $NF}')
-rpmbuild --define "_srcrpmdir $(pwd)" --define "_sourcedir $(pwd)" --nodeps -bs python-redis.spec
-rm -f *.tar.gz
